cfdd5e9dcb5851fc87518919842e13893efbf877
bpm359 <66544508+bpm359@users.noreply.github.com>
2020-06-07 20:19:48 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 console.log("hello")
 
 function getCatImage() {
-    var catImage = document.createElement("img");
-    var source = document.getElementById("image-div");
-    catImage.src="https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
-    source.appendChild(catImage);
+    //location.reload();
+    var catImage = document.getElementById("first-cat-image");
+    //var source = document.getElementById("image-div");
+    catImage.src="https://thecatapi.com/api/images/get?format=src&type=gif&size=small"+new Date().getTime();
+    
+
     
 }
